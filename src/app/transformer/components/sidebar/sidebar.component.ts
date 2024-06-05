fa3063a10107b0fd2bfe76ab6aca7ae413d3306f
Vicent Paya <vicentpayaginer@gmail.com>
2024-06-05 21:16:07 +0200

    feat: drag and drop between the elements in-document and out-document lists

--- src/app/transformer/components/sidebar/sidebar.component.ts
+++ src/app/transformer/components/sidebar/sidebar.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CardPartComponent } from '../card-part/card-part.component';
 import { DocumentPart } from '../../interfaces/DocumentPart.interface';
 import { CardService } from '../../services/card-service.service';
-import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray, DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray, DragDropModule, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-sidebar',
@@ -25,6 +25,10 @@ export class SidebarComponent{
       partTitle: 'Technologies',
       partText: '# Technologies'
     },
+    {
+      partTitle: 'Technologies 2',
+      partText: '# Technologies 2'
+    },
   ];
 
   setEdit(position: number) {
@@ -32,12 +36,20 @@ export class SidebarComponent{
   }
 
   drop(event: CdkDragDrop<DocumentPart[]>) {
-    moveItemInArray(this.cardService.documentParts, event.previousIndex, event.currentIndex);
+    //moveItemInArray(this.cardService.documentParts, event.previousIndex, event.currentIndex);
+
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+
     this.cardService.getTextString();
-  }
-
-  addToDocument(event: any) {
-    console.log(event.target);
-
   }
 }
