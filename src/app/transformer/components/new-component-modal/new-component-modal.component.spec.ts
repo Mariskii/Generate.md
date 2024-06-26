6aad6e43686dc77f92209f9860a8f08225c65322
Vicent Paya <vicentpayaginer@gmail.com>
2024-06-26 20:17:35 +0200

    tests: Added at new-component modal

--- src/app/transformer/components/new-component-modal/new-component-modal.component.spec.ts
+++ src/app/transformer/components/new-component-modal/new-component-modal.component.spec.ts
@@ -1,17 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewComponentModalComponent } from './new-component-modal.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('NewComponentModalComponent', () => {
+const MockDialogRef = {
+  close: () => {}
+}
+
+fdescribe('NewComponentModalComponent', () => {
   let component: NewComponentModalComponent;
   let fixture: ComponentFixture<NewComponentModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NewComponentModalComponent]
+      imports: [
+        NewComponentModalComponent,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {provide: MatDialogRef, useValue: MockDialogRef},
+        {provide: MAT_DIALOG_DATA, useValue: {}}
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(NewComponentModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +32,14 @@ describe('NewComponentModalComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('onNoClick works correctly', () => {
+
+    const spyCloseDialog = spyOn(component.dialogRef, 'close');
+
+    component.onNoClick();
+
+    expect(spyCloseDialog).toHaveBeenCalled();
   });
 });
